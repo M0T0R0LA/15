18280caf1ef77cf700fd172bf9c4457034c59203
M0T0R0LA <58300399+M0T0R0LA@users.noreply.github.com>
2019-12-21 11:55:26 +0300

    знак *

--- script.js
+++ script.js
@@ -16,3 +16,11 @@ function minus(){
    otvet = num1 - num2;
    document.getElementById('result').innerHTML = otvet;   
 }
+function multiplication(){
+   num1 = document.getElementById('first_number').value;
+   num1 = parseInt(num1);
+   num2 = document.getElementById('second_number').value;
+   num2 = parseInt(num2);
+   otvet = num1 * num2;
+   document.getElementById('result').innerHTML = otvet;    
+}
