da9642e8663f2d507b267e51dcfea620e92a8262
M0T0R0LA <58300399+M0T0R0LA@users.noreply.github.com>
2019-12-21 11:58:37 +0300

    знак /

--- script.js
+++ script.js
@@ -24,3 +24,12 @@ function multiplication(){
    otvet = num1 * num2;
    document.getElementById('result').innerHTML = otvet;    
 }
+function division(){
+   num1 = document.getElementById('first_number').value;
+   num1 = parseInt(num1);
+   num2 = document.getElementById('second_number').value;
+   num2 = parseInt(num2);
+   otvet = num1 / num2;
+   document.getElementById('result').innerHTML = otvet;    
+}
+console.log("the calculator is ready");
