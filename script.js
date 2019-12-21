fbf62673601159804322c3ddd6b3987837a744e3
M0T0R0LA <58300399+M0T0R0LA@users.noreply.github.com>
2019-12-21 11:53:03 +0300

    знак -

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-var addition = document.getElementsByName('addition');
+var num1, num2, otvet;
 function plus(){
-   var num1, num2, otvet;
+   
    num1 = document.getElementById('first_number').value;
    num1 = parseInt(num1);
    num2 = document.getElementById('second_number').value;
    num2 = parseInt(num2);
    otvet = num1 + num2;
-   document.getElementById('result').innerHTML = otvet;
-
-
-
-    
+   document.getElementById('result').innerHTML = otvet;   
+}
+function minus(){
+   num1 = document.getElementById('first_number').value;
+   num1 = parseInt(num1);
+   num2 = document.getElementById('second_number').value;
+   num2 = parseInt(num2);
+   otvet = num1 - num2;
+   document.getElementById('result').innerHTML = otvet;   
 }
